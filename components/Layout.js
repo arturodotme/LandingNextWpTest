ff7622c054e64159ce9135156abba528da3298b2
arturodotme <karlozarturojomi@gmail.com>
2022-09-30 12:21:49 -0600

    Modal change click outside

--- components/Layout.js
+++ components/Layout.js
@@ -25,27 +25,10 @@ const customStyles = {
 };
 Modal.setAppElement("#__next");
 
-
 const Layout = ({post,children}) => {
   const {modal} = useLanding()
   return (
     <>
-        <Head>
-            <title>Landing Page Next-WP</title>
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-                integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-                crossorigin="anonymous"
-                referrerpolicy="no-referrer"
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link
-                href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
         <main>{children}</main>
         {modal && (
 				<Modal
@@ -55,7 +38,9 @@ const Layout = ({post,children}) => {
 					<PopUp
           post = {post}/>
 				</Modal>
+        
 			)}
+    
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -9,26 +9,33 @@ import { client } from "../lib/apollo";
 import { gql } from "@apollo/client";
 
 export default function Home({ post }) {
-	const postWp = post.landing1
-	
+	const postWp = post.landing1;
+
 	return (
-		<Layout
-		post = {postWp}
-		>
+		<Layout post={postWp}>
 			<Head>
 				<title>Headless WP Next Starter</title>
 				<link rel="icon" href="favicon.ico"></link>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+					integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+					crossorigin="anonymous"
+					referrerpolicy="no-referrer"
+				/>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link
+					href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 			<main>
 				<div className={styles.container}>
-					<FirstSight
-					post = {postWp} />
-					<Expires
-					post = {postWp} />
-					<Reviews
-					post = {postWp} />
-					<Footer
-					post = {postWp} />
+					<FirstSight post={postWp} />
+					<Expires post={postWp} />
+					<Reviews post={postWp} />
+					<Footer post={postWp} />
 				</div>
 			</main>
 		</Layout>
